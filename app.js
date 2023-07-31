7121eaa73e9c0e5e9c2016b1f10f4df205d4b8e6
Akanle Jesus <jesuswrites20043@gmail.com>
2023-07-31 14:35:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 
 
- const session = require('express-session');
+
 
  // import modules
 
-
+const session = require('express-session')
  const express = require("express");
  const mongoose = require("mongoose");
  const morgan = require("morgan");
  const cors = require("cors");
-
  const path = require('path')
  const bodyparser = require("body-parser");
  const passport = require("passport");
- const bcrypt = require("bcrypt");
- const userRoutes = require("./Routes/users")
  const socketio = require('socket.io');
  const cronCleanup = require('./Controllers/cleanNotifications');
  const env = require('dotenv').config();
@@ -33,14 +30,6 @@
 
 
 
-
-//  const path = require('path')
- const bodyparser = require("body-parser");
-//  const passport = require("passport");
- const bcrypt = require("bcrypt");
-//  const userRoutes = require("./Routes/users")
-
-
  //db
 
  mongoose.connect(env.parsed.MONGO_URI).then((data) => {
@@ -51,18 +40,8 @@
  // middleware
  app.use(express.json());
  app.use[morgan("dev")];
- app.use[cors({ origin: 'https://stale-quiz.onrender.com', credentials: true })];
+ app.use[cors({ origin: 'https://localhost:3000', credentials: true })];
   // app.use[cors({ origin: true, credentials: true })];
-
- app.use("/api/users", userRoutes);
- //routes
- const TestRoutes = require("./Routes/test");
- app.use("/", TestRoutes);
-
-//  app.use("/api/users", userRoutes);
- //routes
-//  const TestRoutes = require("./Routes/test");
-//  app.use("/", TestRoutes);
 
 
  //port
