1c0d030eeaad5bd0150728cb4de4219c1e0bb151
Akanle Jesus <jesuswrites20043@gmail.com>
2023-08-01 00:39:06 +0100

    Update users.js

--- Routes/users.js
+++ Routes/users.js
@@ -3,6 +3,34 @@ import { isLoggedIn, isAdmin } from "../Auth/isLoggedIn"
 const { CreateUser, LoginUser, Logout, GetUser, EditUserName, EditProfilePicture } = require("../Controllers/userController")
 const { GetNotifications } = require("../Controllers/messageController")
 
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+const env = require("dotenv").config();
+
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY ,   
+  api_secret: process.env.CLOUDINARY_API_SECRET   
+});
+
+// Set up multer storage with Cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'Profile-Pictures', 
+    allowed_formats: ['jpg', 'jpeg', 'png'], 
+    transformation: [{ width: 150, height: 150, crop: 'thumb', gravity: 'face' }] 
+  }
+});
+
+
+const upload = multer({ storage: storage });
+
+
 
 
 const router = express.Router()
