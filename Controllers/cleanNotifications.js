ee3842785243021988da372830bf56f52e581b49
Akanle Jesus <jesuswrites20043@gmail.com>
2023-07-31 14:37:48 +0100

    Update cleanNotifications.js

--- Controllers/cleanNotifications.js
+++ Controllers/cleanNotifications.js
@@ -1,15 +1,15 @@
 
 
+const Notification = require("../Models/Notification")
+const cron = require('node-cron')
 
-import cron from 'node-cron'
-import { notificationSchema } from "../Models/Notification";
 
 cron.schedule('0 0 * * *', async () => {
 
   try {
     const oneWeekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
-    await notificationSchema.deleteMany({ timestamp: { $lt: oneWeekAgo } });
-    await notificationSchema.deleteMany({ isRead: true });
+    await Notification.deleteMany({ timestamp: { $lt: oneWeekAgo } });
+    await Notification.deleteMany({ isRead: true });
 
     console.log('Notification cleanup completed.');
   } catch (error) {
