8b08798155a2090f437519a358627a4056fd429c
Akanle Jesus <jesuswrites20043@gmail.com>
2023-08-01 18:01:06 +0100

    Update userValidators.js

--- Validators/userValidators.js
+++ Validators/userValidators.js
@@ -1,51 +1,44 @@
-
-
-let isEmpty = require("is-empty");
-let validator = require("validator");
-
+const isEmpty = require("is-empty");
+const validator = require("validator");
 
 module.exports.LoginValidator = (data) => {
-    const errors = {};
+  const errors = {};
 
-    data.email = !(isEmpty(data.email)) ? data.email : "";
-    data.password = !(isEmpty(data.password)) ? data.password : "";
+  data.email = data.email ? data.email.trim() : "";
+  data.password = data.password ? data.password.trim() : "";
 
-    let emailError = validator.isEmpty(data.email) ? "Email is required!" : (!validator.isEmail(data.email) ? "Please provide a valid email!": "");
-    let passwordError = validator.isEmpty(data.password) ? "Passord is required": "";
+  let emailError = validator.isEmpty(data.email) ? "Email is required!" : !validator.isEmail(data.email) ? "Please provide a valid email!" : "";
+  let passwordError = validator.isEmpty(data.password) ? "Password is required" : "";
 
+  if (emailError) errors.email = emailError;
+  if (passwordError) errors.password = passwordError;
 
-    if (emailError) errors.email = emailError;
-    if (passwordError) errors.password = passwordError;
-
-    return{
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
-
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
 
 module.exports.RegisterValidator = (data) => {
-    const errors = {}
+  const errors = {};
 
-    data.email = !(isEmpty(data.email)) ? data.email : "";
-    data.password = !(isEmpty(data.password)) ? data.password : "";
-    data.firstName = !(isEmpty(data.firstName)) ? data.firstName : "";
-    data.lastName = !(isEmpty(data.lastName)) ? data.lastName : "";
+  data.email = data.email ? data.email.trim() : "";
+  data.password = data.password ? data.password.trim() : "";
+  data.firstName = data.firstName ? data.firstName.trim() : "";
+  data.lastName = data.lastName ? data.lastName.trim() : "";
 
-    let emailError = validator.isEmpty(data.email) ? "Email is required!" : (!validator.isEmail(data.email) ? "Please provide a valid email!": "");
-    let passwordError = validator.isEmpty(data.password) ? "Passord is required": "";
-    let firstNameError = validator.isEmpty(data.firstName) ? "First Name is required": "";
-    let lastNameError = validator.isEmpty(data.lastName) ? "Last Name is required": "";
+  let emailError = validator.isEmpty(data.email) ? "Email is required!" : !validator.isEmail(data.email) ? "Please provide a valid email!" : "";
+  let passwordError = validator.isEmpty(data.password) ? "Password is required" : "";
+  let firstNameError = validator.isEmpty(data.firstName) ? "First Name is required" : "";
+  let lastNameError = validator.isEmpty(data.lastName) ? "Last Name is required" : "";
 
-    if (emailError) errors.email = emailError;
-    if (passwordError) errors.password = passwordError;
-    if (firstNameError || lastNameError) errors.firstName = "Full name is required";
+  if (emailError) errors.email = emailError;
+  if (passwordError) errors.password = passwordError;
+  if (firstNameError) errors.firstName = firstNameError;
+  if (lastNameError) errors.lastName = lastNameError;
 
-    return{
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
-
-
-
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
