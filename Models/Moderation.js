8f7b36bb4434406b3221c0080b963a7b8238b28f
Akanle Jesus <jesuswrites20043@gmail.com>
2023-08-01 02:28:55 +0100

    Update Moderation.js

--- Models/Moderation.js
+++ Models/Moderation.js
@@ -8,13 +8,13 @@ const Schema = Mongoose.Schema
 
   const moderationSchema = new Schema({
     moderatorId: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: Mongoose.Schema.Types.ObjectId,
         ref: 'Users',
         required: true
     },
 
     messageId: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: Mongoose.Schema.Types.ObjectId,
         ref: 'Messages',
         required: true
     },
