c19ef810359d1bed23ca5667389be6365d43e7e3
Akanle Jesus <jesuswrites20043@gmail.com>
2023-08-01 02:46:53 +0100

    God Abeg

--- Controllers/cleanNotifications.js
+++ Controllers/cleanNotifications.js
@@ -1,20 +1,23 @@
+// notificationCleanup.js
+const Notification = require("../Models/Notifications");
+const cron = require('node-cron');
 
+function performNotificationCleanup() {
+  const cronImp = cron.schedule('0 0 * * *', async () => {
+    try {
+      const oneWeekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+      await Notification.deleteMany({ timestamp: { $lt: oneWeekAgo } });
+      await Notification.deleteMany({ isRead: true });
 
-const Notification = require("../Models/Notifications")
-const cron = require('node-cron')
+      console.log('Notification cleanup completed.');
+    } catch (error) {
+      console.error('Notification cleanup error:', error);
+    }
+  });
 
+  cronImp.start();
 
-cron.schedule('0 0 * * *', async () => {
+  return cronImp; // Return the cron job instance to be used in app.js
+}
 
-  try {
-    const oneWeekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
-    await Notification.deleteMany({ timestamp: { $lt: oneWeekAgo } });
-    await Notification.deleteMany({ isRead: true });
-
-    console.log('Notification cleanup completed.');
-  } catch (error) {
-    console.error('Notification cleanup error:', error);
-  }
-});
-
-module.exports = cron;
+module.exports = performNotificationCleanup;
