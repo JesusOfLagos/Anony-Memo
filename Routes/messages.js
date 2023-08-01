0b35de7583d1e556e227244647f31267c319f719
Akanle Jesus <jesuswrites20043@gmail.com>
2023-08-02 00:58:47 +0100

    Update messages.js

--- Routes/messages.js
+++ Routes/messages.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const router = express.Router()
 const { isLoggedIn, isAdmin } = require("../Auth/isLoggedIn")
+const successfullyLoggedIn = require("../Auth/checkAuth")
 const { GetMessages, SearchMessages, ReportMessageAbuse, ModerateMessage, SendMessage } = require("../Controllers/messageController")
+const SendNotification = require("../Controllers/notificationController")
 
 
 
-router.get('/users/get-all-messages', isLoggedIn, GetMessages)
-router.post('/messages/search', isLoggedIn, SearchMessages)
-router.post('/users/send-a-message', isLoggedIn, SendMessage)
-router.post('/messages/report/:id', isLoggedIn, ReportMessageAbuse)
-router.post('/messages/moderate/:id', isAdmin, ModerateMessage)
+router.get('/users/get-all-messages/:_id', GetMessages)
+router.post('/messages/search', SearchMessages)
+router.post('/users/send-a-message/:_id', SendMessage)
+router.post('/messages/report/:_id', ReportMessageAbuse)
+router.post('/messages/moderate', ModerateMessage)
+router.post('/users/send-notification/:_id', SendNotification)
 
 
 module.exports = router;
